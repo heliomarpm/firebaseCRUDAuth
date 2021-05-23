e1dc3cd6faa1734be6a79714013f739bce2e6222
Heliomar P. Marques <heliomarpm@gmail.com>
2021-05-23 14:59:47 -0300

    first commit

---
+++ src/environments/app.environment.ts
@@ -0,0 +1,10 @@
+export const appEnvironment = {
+    firebaseConfig: {
+        apiKey: 'AIzaSyCFuu3gT90Og5hvdtzZH_5x4jLATZxy7s8',
+        authDomain: 'pocs-85eae.firebaseapp.com',
+        projectId: 'pocs-85eae',
+        storageBucket: 'pocs-85eae.appspot.com',
+        messagingSenderId: '686311252525',
+        appId: '1:686311252525:web:c4398b9dfc2e9121bdcea4'
+    }
+};
